c3648c07da7d35fef4a7d9b316702110f78b7865
LucasNithael <lucasnithael0101@gmail.com>
2023-12-27 00:44:21 -0300

    validator FluentValidation

--- SquadManager/Controllers/LoginController.cs
+++ SquadManager/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using SquadManager.Models;
+using SquadManager.Validator;
 
 namespace SquadManager.Controllers
 {
@@ -8,6 +10,21 @@ namespace SquadManager.Controllers
         public IActionResult Index()
         {
             UserViewModel user = new UserViewModel();
+
+            user.Email = "lucasnithael32@gmail.com";
+
+            UserValidator validator = new UserValidator();
+
+            ValidationResult results = validator.Validate(user);
+
+            if(!results.IsValid)
+            {
+                foreach(var item in results.Errors)
+                {
+                    Console.WriteLine(item.ErrorMessage);
+                }
+            }
+
             return View("Index", user);
         }
         [HttpPost]
--- SquadManager/Validator/UserValidator.cs
+++ SquadManager/Validator/UserValidator.cs
@@ -7,9 +7,10 @@ namespace SquadManager.Validator
     {
         public UserValidator()
         {
-            RuleFor(x => x.UserName).NotEmpty().WithMessage("O campo nome é obrigatório");
-            RuleFor(x => x.Email).NotEmpty().WithMessage("O campo email é obrigatório");
-            RuleFor(x => x.Password).NotEmpty().WithMessage("O campo senha é obrigatório");
+            RuleFor(x => x.UserName).NotNull().WithMessage("O campo nome é obrigatório");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email inválido");
+            RuleFor(x => x.Email).NotNull().WithMessage("O campo email é obrigatório");
+            RuleFor(x => x.Password).NotNull().WithMessage("O campo senha é obrigatório");
         }
     }
 }
