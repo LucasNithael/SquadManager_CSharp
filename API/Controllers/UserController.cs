ff410d9b34f281995db15915415be454a57aeefb
LucasNithael <lucasnithael0101@gmail.com>
2023-12-27 02:00:33 -0300

    Rest Api .Net Core

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login(string username, string passowrd)
+        {
+            return Ok(new { response = $"{username} está logado"});
+        }
+    }
+}
